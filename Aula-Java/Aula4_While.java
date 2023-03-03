e2183207e694d5ecd5321501a4c6bfee5e25f32c
Luis Guilherme Castro de Melo <104398213+LuisGuilherme1996@users.noreply.github.com>
2023-03-03 01:30:23 +0000

    aula 5

---
+++ Aula-Java/Aula4_While.java
@@ -0,0 +1,10 @@
+public class Aula4_While{
+    public static void main(String[] args){
+        int contador = -101;
+        while(contador <= 1){
+            System.out.println(contador);
+            contador++;
+
+        }
+    }
+}
---
+++ Aula-Java/Exercicio5.java
@@ -0,0 +1,13 @@
+public class Exercicio5{
+    public static void main(String[] args){
+        String nome = "Luis";
+        int telefone = 991319006;
+        String rua = "Herminio Cavallari";
+        int numeroDaCasa = 335;
+        String enedereco = rua + numeroDaCasa;
+        System.out.println(nome);
+        System.out.println(telefone);
+        // System.out.println(rua);
+        System.out.println(enedereco);
+    }
+}
